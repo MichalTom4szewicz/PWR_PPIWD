ec85f193914fe0ed4248f669be907664d7e617d7
jakubfaldasz <jakub.faldasz@icloud.com>
2022-03-22 21:50:33 +0100

    [BACK-009] Endpoint /training/measurement/<activity>

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -3,10 +3,11 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
+from flask_restful import Api, Resource, reqparse
 from server.services.UserService import UserService
 
 from server.models.User import User
-
+from server.blueprints.Training import Training
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -16,5 +17,7 @@ def create_app():
         'db': 'ppiwd'
     }
     MongoEngine(app)
+
+    app.register_blueprint(Training)
 
     return app
---
+++ backend/server/blueprints/Training.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, Response, request, jsonify
+from server.services.DatasetService import DatasetService
+from server.config import config
+
+Training = Blueprint('training', __name__)
+
+dataset_service = DatasetService(config.training_dataset.dataset_dir, config.training_dataset.backup_dir)
+
+@Training.route('/training/measurement/<activity>', methods=['POST'])
+def save_training_measurements(activity):
+        if 'measurements' not in request.files:
+                res = {'errorMessage': 'No \'measurements\' file part'}
+                return res, 400
+        csv_file = request.files['measurements']
+        if not csv_file.filename.endswith(('.csv', '.tsv')):
+                res = {'errorMessage': 'File format should be csv or tsv'}
+                return res, 400
+        csv_data = csv_file.read().decode("utf-8") 
+        dataset_service.saveMeasurement(activity, csv_data)
+        res = {'message': 'CSV/TSV uploaded'}
+        return res, 200
